44e37b078db44dadfe11de1a6101fc7fc9f2142b
Venice <vmg24@ic.ac.uk>
2025-11-30 18:18:31 +0000

    update to reg file for pipe

--- rtl/ME_WR_Reg.sv
+++ rtl/ME_WR_Reg.sv
@@ -9,9 +9,15 @@ module ME_WR_reg #(
     input logic [DATA_WIDTH-1:0] ALUOutM,
     input logic [DATA_WIDTH-1:0] ReadDataM,
     input logic [4:0]           WriteRegM,
+    input  logic [1:0] ResultSrcM,
+    input  logic [DATA_WIDTH-1:0] ExtImmM,
+    input  logic [DATA_WIDTH-1:0] PCPlus4M,
 
     output logic                RegWriteW,
     output logic                MemtoRegW,
+    output logic [1:0] ResultSrcW,
+    output logic [DATA_WIDTH-1:0] ExtImmW,
+    output logic [DATA_WIDTH-1:0] PCPlus4W,
     output logic [DATA_WIDTH-1:0] ALUOutW,
     output logic [DATA_WIDTH-1:0] ReadDataW,
     output logic [4:0]          WriteRegW 
@@ -23,6 +29,9 @@ module ME_WR_reg #(
             ALUOutW <= 0;
             ReadDataW <= 0;
             WriteRegW <= 0;
+            ResultSrcW  <= 0;      
+            ExtImmW     <= 0;
+            PCPlus4W    <= 0;
         end 
         else begin
             RegWriteW  <= RegWriteM;
@@ -30,6 +39,9 @@ module ME_WR_reg #(
             ALUOutW    <= ALUOutM;
             ReadDataW  <= ReadDataM;
             WriteRegW  <= WriteRegM;
+            ResultSrcW <= ResultSrcM;
+            ExtImmW    <= ExtImmM;
+            PCPlus4W   <= PCPlus4M;
         end
     end
 
